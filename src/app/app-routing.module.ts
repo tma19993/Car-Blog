788546bc5c1046f3555b91e0f882864f996f966b
Tomasz Mazurek <tmazzurek@gmail.com>
2022-07-10 22:53:17 +0200

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { WeatherComponent } from './weather/weather.component';
+import { WritePostComponent } from './write-post/write-post.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'weather', component: WeatherComponent },
+  { path: 'write-post', component: WritePostComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { WritePostComponent } from './write-post/write-post.component';
+import { AboutComponent } from './about/about.component';
+import { NavigatorComponent } from './navigator/navigator.component';
+import { WeatherComponent } from './weather/weather.component';
+
+import { FormsModule } from '@angular/forms';
+
+import { PostsService } from './services/posts.service';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WritePostComponent,
+    AboutComponent,
+    NavigatorComponent,
+    WeatherComponent,
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  providers: [PostsService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
